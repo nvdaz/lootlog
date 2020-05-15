796e79e8ecb568069b24dc6b727edd904a97f434
Kyle <kawigdd@gmail.com>
2020-05-15 16:29:49 -0400

    remove dist

--- dist/auth/passport.js
+++
@@ -1,6 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _passport = _interopRequireDefault(require("passport"));
-var _strategy = _interopRequireDefault(require("./strategy"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-_passport.default.use(_strategy.default);var _default =
-
-_passport.default;exports.default = _default;
